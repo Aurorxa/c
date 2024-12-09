7665511e950497efbc5e166424448eddb3b23060
许大仙 <1900919313@qq.com>
2024-12-09 04:17:27 +0000

    2024年12月9日 12:17

--- docs/.vitepress/config/share.ts
+++ docs/.vitepress/config/share.ts
@@ -105,5 +105,9 @@ export const sharedConfig = defineConfig({
   },
   themeConfig: { // 主题设置
     logo: '/logo.svg',  // 左上角logo
+    editLink: {
+      pattern: 'https://github.com/vuejs/vitepress/edit/main/docs/:path',
+      text: 'Edit this page on GitHub'
+    }
   }
 })
