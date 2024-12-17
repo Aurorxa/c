cd54ea69d0cb14d71004d999b6ee4d7be733b4b9
许大仙 <1900919313@qq.com>
2024-12-17 15:54:41 +0800

    增加进度条

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -8,9 +8,12 @@ import { useData, useRoute, inBrowser } from 'vitepress'
 import Confetti from "./components/Confetti.vue"
 import SwitchLayout from './components/SwitchLayout.vue'
 import HomeUnderline from "./components/HomeUnderline.vue"
+import 'nprogress-v2/dist/index.css'
 import "vitepress-markdown-timeline/dist/theme/index.css"
 import 'virtual:group-icons.css' //代码组样式
 import './style/index.css'
+
+import { NProgress } from 'nprogress-v2/dist/index.js'
 
 export default {
   extends: DefaultTheme,
@@ -21,9 +24,19 @@ export default {
     app.component('ArticleMetadata', ArticleMetadata)
     app.component('confetti', Confetti)
     app.component('HomeUnderline', HomeUnderline)
+    if (inBrowser) {
+      NProgress.configure({ showSpinner: false })
+      // 手动定义 onBeforeRouteChange
+      router.onBeforeRouteChange = () => {
+        NProgress.start() // 开始进度条
+      }
+      // 在页面加载完成时停止进度条
+      router.onAfterRouteChanged = () => {
+        NProgress.done() // 停止进度条
+      }
+    }
   },
   setup() {
-    // Get frontmatter and route
     const { frontmatter } = useData()
     const route = useRoute()
     const initZoom = () => {
